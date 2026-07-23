a1b7a0682e797830cdb08baca12265cd1c736b73
YusufSevenn <159650914+YusufSevenn@users.noreply.github.com>
2026-07-23 14:05:23 +0300

    CategoryController dosyası yazıldı.

---
+++ ECommerce.API/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.Core.DTOs;
+using ECommerce.Interfaces;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CategoryCreateDto categoryCreateDto)
+        {
+            var isSuccess = await _categoryService.CreateCategoryAsync(categoryCreateDto);
+
+            if (isSuccess)
+            {
+                return Ok("Kategori başarıyla eklendi.");
+            }
+            return BadRequest("Kategori eklenirken bir hata oluştu.");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            var categoryDto = await _categoryService.GetCategoryByIdAsync(id);
+
+            if (categoryDto == null) return NotFound("Ürün bulunamadı.");
+
+            return Ok(categoryDto);
+        }
+    }
+}
